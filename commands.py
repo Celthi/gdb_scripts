6c8972f43d74c658699eb550aa5c1f5f8829be9e
xialu <xialu@microstrategy.com>
2022-03-27 13:50:12 +0800

    add stl

--- commands.py
+++ commands.py
@@ -15,3 +15,33 @@ class SampleCommand(gdb.Command):
 
     def invoke(self, argument, from_tty):
         print('command is called.')
+
+
+import argparse
+
+
+class DTCommand(gdb.Command):
+    def __init__(self):
+        # This registers our class as "simple_command"
+        super(DTCommand, self).__init__("dt", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        # When we call "simple_command" from gdb, this is the method
+        # that will be called.
+        args = arg.split()
+        parser = argparse.ArgumentParser(
+            description='print the type and offset of fields')
+        parser.add_argument('-t', dest='ty', action='store',
+                            default='',
+                            help='provide the type name')
+
+        args = parser.parse_args(args)
+        ty = args.ty
+        typ = gdb.lookup_type(ty)
+        for field in typ.fields():
+            print('+{} {}'.format(field.bitpos/8, field.name))
+        
+
+
+        
+DTCommand()
