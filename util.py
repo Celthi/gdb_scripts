91bd4d30e753da92ab32135b716162750a7ec8b7
xialu <xialu@microstrategy.com>
2022-04-04 22:48:57 +0800

    add util

--- util.py
+++ util.py
@@ -2,6 +2,9 @@ try:
     import gdb
 except ImportError as e:
     raise ImportError("This script must be run in GDB: ", str(e))
+    
+from libstdcxx.v6.printers import StdMapPrinter, StdVectorPrinter, StdListPrinter
+import re
 
 
 def print_stack_trace():
@@ -20,3 +23,41 @@ def norm_type(tp):
             return norm_type(pointee_type)
         return pointee_type
     return tp
+
+
+def StdMapToDict(iMap):
+    it = StdMapPrinter('map', iMap).children()
+    res = {}
+    for key, value in it:
+        m = re.search('\[(\d{1,3})\]', key)
+        if m:
+            count = int(m.group(1))
+            if count % 2 == 0:
+                # it is key
+                k = '{}'.format(value)
+            else:          
+                res[k] = value.referenced_value()
+    return res
+
+def stdVectorToPyList(typename, vector):
+    '''
+    Given std::vector in gdb.Value, return a list of gdb.Value of the list's elements
+    '''
+    rslist = []
+    elements = StdVectorPrinter(typename, vector).children()
+    for (_, val) in iter(elements):
+        rslist.append(val)
+
+    return rslist
+
+def stdListToPyList(typename, list):
+    '''
+    Given std::list in gdb.Value, return a list of gdb.Value of the list's elements
+    '''
+    rslist = []
+    nodes = StdListPrinter(typename, list).children()
+    for (_, val) in iter(nodes):
+        rslist.append(val)
+
+    return rslist
+
