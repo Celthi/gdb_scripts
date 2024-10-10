281127cc736d680fcf9a3eee7162c3a2feb26cd5
Evan Lu <txtlu0@gmail.com>
2024-10-10 12:44:46 +0000

    fix import error

--- prelude.py
+++ prelude.py
@@ -1,12 +1,8 @@
 import sys
 import os
-def add_paths():
-    pythondir = '/usr/share/gcc/python'
-    print('add to the python sys.path: '+ pythondir)
-    sys.path.append(pythondir)
-    scripts_folder = os.path.dirname(__file__)
-
+def add_paths(file_name):
+    scripts_folder = os.path.dirname(file_name)
     print('add to the python sys.path: '+ scripts_folder)
     sys.path.insert(0, scripts_folder)
 
-add_paths()
+add_paths(__file__)
